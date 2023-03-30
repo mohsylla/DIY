bb392336b4598cf06418c6a3d065afcff10012e6
= <sylla.m7pro@gmail.com>
2023-03-30 15:48:23 +0200

    ajout d'un carousel

--- DIY/ViewController.swift
+++ DIY/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  DIY
-//
-//  Created by Mo on 30/03/2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet var collectionView: UICollectionView!
+
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Créer une UICollectionView avec un layout horizontal
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        //let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        //view.addSubview(collectionView)
     }
 
+    // MARK: - UICollectionViewDataSource
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+
+    // MARK: - UICollectionViewDelegateFlowLayout
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
 
 }
-
